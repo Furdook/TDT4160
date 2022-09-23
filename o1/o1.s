0a3515c1e1d74777617dccee5309c8585e9f3cc0
Tai <timonas@stud.ntnu.no>
2022-09-23 13:04:56 +0200

    cleanup o1

--- o1/o1.s
+++ o1/o1.s
@@ -12,23 +12,23 @@ Start:
     ldr r0, =GPIO_BASE + (PORT_SIZE * LED_PORT)  // 0x40006000 + ( 36 * 4 )
     ldr r1, =GPIO_PORT_DOUTCLR  // 20
     ldr r2, =GPIO_PORT_DOUTSET  // 16
-    ldr r3, =GPIO_PORT_DIN
+    ldr r3, =GPIO_PORT_DIN      // 28
 
-    add r1, r0, r1  // clear      - r0 + 20
-    add r2, r0, r2  // set        - r0 + 16
-    add r3, r0, r3  // data input - r0 + 28
+    add r1, r0, r1  // clear
+    add r2, r0, r2 	// set
+    add r3, r0, r3  // data input
 
     // << is the same as LSL
     ldr r4, =1 << LED_PIN     // 2
     ldr r5, =1 << BUTTON_PIN  // 9
 
 Loop:
-    ldr r6, [r3]  // last data input på r6?
-    and r3, r5    // kok
-    cmp r3, r5
-    beq On        // jump to On
+    //ldr r6, [r3]  // last data input på r6?
+    and r6, r3, r5  // add datainput with buttonpin
+    cmp r3, r5		// compare to determain whether pushed or not
+    beq On          // jump to On if cmp = True
 
-    str r4, [r2]  // turn off?
+    str r4, [r2]    // else turn off?
     B Loop
 
 On:
