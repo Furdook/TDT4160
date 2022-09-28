73752c694185260335908e02c8b6843d03c21ff2
Tai <timonas@stud.ntnu.no>
2022-09-28 16:01:59 +0200

    cleanup

--- o1/o1.s
+++ o1/o1.s
@@ -1,36 +1,37 @@
 .thumb
 .syntax unified
 
-.include "gpio_constants.s"     // Register-adresser og konstanter for GPIO
+.include "gpio_constants.s"
 
 .text
 	.global Start
 	
 Start:
-
-    // Skriv din kode her...
     ldr r0, =GPIO_BASE + (PORT_SIZE * LED_PORT)  // 0x40006000 + ( 36 * 4 )
+    ldr r4, =GPIO_BASE + PORT_SIZE               // 0x40006000 + 36
+
     ldr r1, =GPIO_PORT_DOUTCLR  // 20
     ldr r2, =GPIO_PORT_DOUTSET  // 16
-    ldr r3, =GPIO_BASE + (PORT_SIZE * BUTTON_PORT) + GPIO_PORT_DIN  // Data Input
+    ldr r3, =GPIO_PORT_DIN      // 38
 
-    add r1, r0, r1  // clear
-    add r2, r0, r2 	// set
+    add r1, r0, r1  // Clear
+    add r2, r0, r2 	// Set
+    add r3, r4, r3  // Data input
 
-	mov r4, #1 << LED_PIN     //  LEDleft shift to 100
-	mov r5, #1 << BUTTON_PIN  // left shift to 10000000
+	mov r0, #1 << LED_PIN     // LED_PIN = 2,    so we get 0b100
+	mov r4, #1 << BUTTON_PIN  // BUTTON_PIN = 9, so we get 0b100000000
 
 Loop:
-	ldr r6, [r3]
-    and r6, r5, r6
-    cmp r6, r5		// compare to determain whether pushed or not
-    beq On          // jump to On if cmp = True
+	ldr r5, [r3]    // load data input value on r6
+    and r5, r4, r5  // and to get intended data input
+    cmp r5, r4		// compare to determain whether pushed or not
+    beq On          // jump to On if cmp true
 
-    str r4, [r2]    // else turn off
-    B Loop
+    str r0, [r2]    // str set on LED, turns off
+    B Loop          // restarts loops
 
 On:
-    str r4, [r1]
-    B Loop
+    str r0, [r1]    // str clear on LED, turns on
+    B Loop          // restarts loop
 
-NOP // Behold denne på bunnen av fila
+NOP
