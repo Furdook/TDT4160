88013d9e9a8d123fbb392eb95e492de79bf6b6bf
Tai <timonas@stud.ntnu.no>
2022-09-29 09:14:22 +0200

    Fix spacing (git) ?

--- o1/o1.s
+++ o1/o1.s
@@ -19,12 +19,12 @@ Start:
     add r3, r4, r3  // Data input
 
     // As we want the pins below we shift a 1 to the index of LED_PIN and BUTTON_PIN (Think of the microcontroller)
-	// lsl can also be used to acheive this, alternatively write out the address manually, ie: mov r0, 0b100
+    // lsl can also be used to acheive this, alternatively write out the address manually, ie: mov r0, 0b100
     mov r0, #1 << LED_PIN     // LED_PIN = 2,    so we get 0100
-	mov r4, #1 << BUTTON_PIN  // BUTTON_PIN = 9, so we get 0001 0000 0000
+    mov r4, #1 << BUTTON_PIN  // BUTTON_PIN = 9, so we get 0001 0000 0000
 
 Loop:
-	ldr r5, [r3]    // load data input value on r5 from r3
+    ldr r5, [r3]    // load data input value on r5 from r3
     and r5, r4, r5  // we want to know what the input r4 is when btn0 is pressed, r4 and r5 is this value
     cmp r5, r4		// compare to determain whether pushed or not, if pushed r4 will be the value of r5
     beq On          // jump to On if cmp true
